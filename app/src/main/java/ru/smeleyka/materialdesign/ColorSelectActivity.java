c10fa9ddf8916a8e10c9601ce80a1aeb5b2d6577
smeleyka <smeleyka@gmail.com>
2018-01-30 17:59:28 +0300

    Initial commit

---
+++ app/src/main/java/ru/smeleyka/materialdesign/ColorSelectActivity.java
@@ -0,0 +1,13 @@
+package ru.smeleyka.materialdesign;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ColorSelectActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_select);
+    }
+}
